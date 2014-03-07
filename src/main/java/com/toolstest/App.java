7f10d30899a408f2b942a3d0eb02be45a045df4a
akalanka <akalanka@akalanka-HP-ProBook-450-G0.(none)>
2014-03-07 18:16:07 +0530

    Initial commit

---
+++ src/main/java/com/toolstest/App.java
@@ -0,0 +1,13 @@
+package com.toolstest;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
