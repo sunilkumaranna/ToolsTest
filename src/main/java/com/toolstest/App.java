d0e07af914e8e60bcabec5435b30916b9da113a2
sunilkumaranna <47133733+sunilkumaranna@users.noreply.github.com>
2019-10-31 08:22:00 +0530

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -4,6 +4,8 @@ package com.toolstest;
  * Hello world!
  */
 public class App {
+    
+    private string name;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
